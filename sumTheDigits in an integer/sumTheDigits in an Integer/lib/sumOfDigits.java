d4b4ed975e66ee1217be92693749c61927c4bd4d
Anthonyruiz85 <anthonyruiz85@outlook.com>
2023-11-18 22:15:27 -0500

    test

---
+++ sumTheDigits in an integer/sumTheDigits in an Integer/lib/sumOfDigits.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class sumOfDigits {
+    public static void main(String[] args) throws Exception {
+    Scanner userInput = new Scanner(System.in);
+    System.out.printf("enter number you wish to evaluate: ");
+    long num = userInput.nextInt();// stores userInput in num
+      
+    if (num<0){
+    System.out.println("please enter a positive number");
+    return;
+}
+    int results =sumDigits(num);
+    System.out.println("the sum of numbers is " + results);
+
+      
+      
+    }
+    
+      public static int sumDigits(long n){
+   int sumDigits=0;
+        while (n>0) {
+
+
+int moduleo = (int)(n%10);
+sumDigits += moduleo;
+n = n/10;
+   }
+return sumDigits;
+
+}
+    }
+
