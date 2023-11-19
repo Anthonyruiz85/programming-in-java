94b2f8e99d968ae5b351ad8647a1da827b06eaff
Anthonyruiz85 <anthonyruiz85@outlook.com>
2023-11-18 22:09:11 -0500

    test

---
+++ milesToKilometers loop/miles to kilometers/lib/app.java
@@ -0,0 +1,16 @@
+
+    
+public class app {
+    public static void main(String[] args) throws Exception {
+       int miles=1;
+       
+       System.out.println("miles\t"+ "kilometers");
+       
+        for(miles=1;miles<=10;miles++){
+
+double kilometers = miles*1.609;        
+System.out.println(miles+"\t"+kilometers);// /t adds space
+    }
+  }
+}
+
