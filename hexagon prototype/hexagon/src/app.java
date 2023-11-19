3716b91cc1330e108d2069b41a8151b99b5618f7
Anthonyruiz85 <anthonyruiz85@outlook.com>
2023-11-18 22:03:02 -0500

    test

---
+++ hexagon prototype/hexagon/src/app.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class app {
+   private static Scanner input = new Scanner(System.in);
+
+public static void main(String[]args) throws Exception {
+
+System.out.println("please enter  the legnth of a side of a Hexagon:");
+//double user input = input.nextDouble
+
+Hexagon userHexagon = new Hexagon(7);
+Hexagon userHexagonTwo = new Hexagon(-175.5547);
+
+System.out.println(userHexagon.CalculateAreaOfHexagonWithOneSide());
+System.out.println(userHexagonTwo.CalculateAreaOfHexagonWithOneSide());
+ } 
+}
